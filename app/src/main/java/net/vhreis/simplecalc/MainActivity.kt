308299fcaf3a73ef37a94d352a6299cb9263b898
vhreis <victor.h.s.reis@gmail.com>
2023-04-11 21:30:30 -0300

    Layout and initial of logic

--- app/src/main/java/net/vhreis/simplecalc/MainActivity.kt
+++ app/src/main/java/net/vhreis/simplecalc/MainActivity.kt
@@ -2,10 +2,19 @@ package net.vhreis.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View){
+        Toast.makeText(this, "Você clicou",Toast.LENGTH_LONG ).show()
+    }
+
+    fun onOperator(view: View){
+        Toast.makeText(this,"Você clicou em uma operação",Toast.LENGTH_LONG).show()
     }
 }
