3c45f978efe1b33af78f8c28c1e3949bcc22e525
vhreis <victor.h.s.reis@gmail.com>
2023-04-11 23:27:37 -0300

    end of logic of operations

--- app/src/main/java/net/vhreis/simplecalc/MainActivity.kt
+++ app/src/main/java/net/vhreis/simplecalc/MainActivity.kt
@@ -3,18 +3,165 @@ package net.vhreis.simplecalc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var txtInput: TextView? = null
+    private var lastNumeric : Boolean = true
+    private var lastDot : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtInput = findViewById(R.id.txtInput)
     }
     fun onDigit(view: View){
-        Toast.makeText(this, "Você clicou",Toast.LENGTH_LONG ).show()
+        val btn = view as Button
+        txtInput?.append(btn.text)
+        lastNumeric = true
     }
 
     fun onOperator(view: View){
-        Toast.makeText(this,"Você clicou em uma operação",Toast.LENGTH_LONG).show()
+        txtInput?.text?.let{
+            if(lastNumeric && !isOperationAdded(it.toString())){
+                txtInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+
+
+            }
+        }
+    }
+    private fun isOperationAdded(value: String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{ value.contains("/")
+                || value.contains("*")
+                || value.contains("+")
+                || value.contains("-")
+        }
+    }
+    fun onEqual(view: View) {
+        val txtValue = txtInput?.text.toString()
+
+        if(txtValue.contains("Ininito")){
+            txtInput?.text = "0"
+            Toast.makeText(this,"Resultado Zerado, Não se pode dividir por zero",Toast.LENGTH_LONG).show()
+        }
+        if(lastNumeric){
+            when {
+                txtValue.contains("-") -> subtract(txtValue)
+                txtValue.contains("+") -> sum(txtValue)
+                txtValue.contains("*") -> multiply(txtValue)
+                txtValue.contains("/") -> divide(txtValue)
+            }
+        }
+    }
+
+    fun onClear(view: View) {
+        txtInput?.text = ""
+        lastDot = false
+        lastNumeric = false
+    }
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            txtInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+    private fun subtract(txtValue: String){
+        var prefix = ""
+        var txtValue2:String = txtValue
+        try {
+            if(txtValue2.startsWith("-")){
+                prefix = "-"
+                txtValue2 = txtValue.substring(1)
+            }
+            if (txtValue.contains("-")){
+                val split = txtValue2.split("-")
+                var one = split[0]
+                val two = split[1]
+                if(prefix.isNotEmpty()){
+                    one = prefix + one
+                }
+                txtInput?.text = (one.toDouble() - two.toDouble()).toString()
+            }
+
+        }catch (e: ArithmeticException){
+            e.printStackTrace()
+        }
+    }
+    private fun sum(txtValue: String){
+        var prefix = ""
+        var txtValue2:String = txtValue
+        try {
+            if(txtValue2.startsWith("-")){
+                prefix = "-"
+                txtValue2 = txtValue.substring(1)
+            }
+            if (txtValue.contains("+")){
+                val split = txtValue2.split("+")
+                var one = split[0]
+                val two = split[1]
+                if(prefix.isNotEmpty()){
+                    one = prefix + one
+                }
+                (one.toDouble() + two.toDouble()).toString().also { txtInput?.text = it }
+            }
+
+        }catch (e: ArithmeticException){
+            e.printStackTrace()
+        }
+    }
+    private fun multiply(txtValue: String){
+        var prefix = ""
+        var txtValue2:String = txtValue
+        try {
+            if(txtValue2.startsWith("-")){
+                prefix = "-"
+                txtValue2 = txtValue.substring(1)
+            }
+            if (txtValue.contains("*")){
+                val split = txtValue2.split("*")
+                var one = split[0]
+                val two = split[1]
+                if(prefix.isNotEmpty()){
+                    one = prefix + one
+                }
+                txtInput?.text = (one.toDouble() * two.toDouble()).toString()
+            }
+
+        }catch (e: ArithmeticException){
+            e.printStackTrace()
+        }
+    }
+    private fun divide(txtValue: String){
+        var prefix = ""
+        var txtValue2:String = txtValue
+        try {
+            if(txtValue2.startsWith("-")){
+                prefix = "-"
+                txtValue2 = txtValue.substring(1)
+            }
+            if (txtValue.contains("/")){
+                val split = txtValue2.split("/")
+                var one = split[0]
+                val two = split[1]
+                if(two.toDouble() == 0.0){
+                    txtInput?.text="Ininito"
+                    lastNumeric= false
+                    return
+                }
+                if(prefix.isNotEmpty()){
+                    one = prefix + one
+                }
+                txtInput?.text = (one.toDouble() / two.toDouble()).toString()
+            }
+
+        }catch (e: ArithmeticException){
+            e.printStackTrace()
+        }
     }
 }
